c9e17ba0b5cdd6f98e14e034a4f4c21c2b8ca7d9
Caroline Desprat <caroline.desprat@gmail.com>
2014-10-08 15:07:44 +0200

    separation en fonction --working

--- main.cpp
+++ main.cpp
@@ -18,30 +18,33 @@ inline T dot(const vector<T>& v1,const vector<T>& v2)
 template<class T>
 inline T determinant(const vector<T>& u,const vector<T>& v,const vector<T>& w)
 {return dot(u,cross(v,w));}
+// Mesh type
+typedef OpenMesh::TriMesh_ArrayKernelT<> MyMesh;
 
-int main(int argc, char *argv[]){
-
-    int decimate_to=1000;
-    decimate_to =atoi(argv[1]);
-
-    printf ("decimate to %d vertices\n", decimate_to);
-    // ---------------------------------------- necessary types
-    // Mesh type
-    typedef OpenMesh::TriMesh_ArrayKernelT<> MyMesh;
-    // Decimater type
-    typedef OpenMesh::Decimater::DecimaterT< MyMesh > Decimater;
-    // Decimation Module Handle type
-    typedef OpenMesh::Decimater::ModQuadricT< MyMesh >::Handle HModQuadric;
-    // ---------------------------------------- decimater setup
-    MyMesh mesh; // a mesh object
-    std::string filename = "fandisk.obj";
-
-    OpenMesh::IO::Options opt;
-    if (!OpenMesh::IO::read_mesh(mesh, filename,opt))
+void writeMesh(MyMesh mesh,std::string outputFilename){
+    if (!OpenMesh::IO::write_mesh(mesh, outputFilename))
     {
-        std::cerr << "read error\n";
+        std::cerr << "write error\n";
         exit(1);
     }
+    cout << "Output file created : "<< outputFilename<<endl;
+}
+MyMesh cleanMesh(MyMesh mesh){
+    int i=0;
+    for (MyMesh::FaceIter f_it=mesh.faces_begin(); f_it!=mesh.faces_end(); ++f_it){
+        if (mesh.valence(f_it.handle())==0){
+            mesh.delete_face(f_it.handle(),false);
+            i++;
+        }
+    }
+    mesh.garbage_collection();
+
+    std::cout << "Faces deleted " << i++ << std::endl;
+    return mesh;
+}
+
+
+float computeMeshVolume(MyMesh mesh){
     float sum=0;
     for (MyMesh::FaceIter f_it=mesh.faces_begin(); f_it!=mesh.faces_end(); ++f_it){
 
@@ -55,11 +58,39 @@ int main(int argc, char *argv[]){
         }
         sum+= determinant(face[0],face[1],face[2]);
     }
+    return sum;
+}
 
-    cout << "sum " <<sum <<std::endl;
+
+MyMesh loadMesh(std::string filename){
+    // ---------------------------------------- necessary types
+
+    // ---------------------------------------- decimater setup
+    MyMesh mesh; // a mesh object
+
+    //std::string filename = "fandisk.obj";
+
+    OpenMesh::IO::Options opt;
+    if (!OpenMesh::IO::read_mesh(mesh, filename,opt))
+    {
+        std::cerr << "read error\n";
+        exit(1);
+    }
+
+    return mesh;
+}
+
+void createLOD(std::string filename,float percent){
+
+    MyMesh mesh=loadMesh(filename);
+    float sum1 = computeMeshVolume(mesh);
+    // Decimater type
+    typedef OpenMesh::Decimater::DecimaterT< MyMesh > Decimater;
+    // Decimation Module Handle type
+    typedef OpenMesh::Decimater::ModQuadricT< MyMesh >::Handle HModQuadric;
+
     Decimater decimater(mesh); // a decimater object, connected to a mesh
 
-    cout << decimater.mesh().n_vertices()<<endl;
     HModQuadric hModQuadric; // use a quadric module
     decimater.add(hModQuadric); // register module at the decimater
     // the way to access the module
@@ -71,42 +102,39 @@ int main(int argc, char *argv[]){
     decimater.module( hModQuadric ).unset_max_err();
     decimater.initialize(); // let the decimater initialize the mesh and the
     // modules
+    float vertD = roundf(percent*decimater.mesh().n_vertices());
+    decimater.decimate_to(vertD);
 
-    decimater.decimate_to(500);
-    int i=0;
-    float sum2=0;
-    for (MyMesh::FaceIter f_it=mesh.faces_begin(); f_it!=mesh.faces_end(); ++f_it){
+    printf ("decimate to %f percent (%f v) of %zu vertices\n", percent, vertD,decimater.mesh().n_vertices());
+
+    mesh=cleanMesh(mesh);
+    float sum2 = computeMeshVolume(mesh);
+    float diff=sum1 - sum2;
+    printf("difference sum (%f) - sum 2 (%f) : %f\n",sum1,sum2,diff);
+
+    string output= to_string(percent)+filename+"";
+    writeMesh(mesh,output);
+
+}
 
 
 
-        if (mesh.valence(f_it.handle())==0){
-            mesh.delete_face(f_it.handle(),false);
-            i++;
-        }else{
-            std::vector< std::vector<float> > face ;
-            for (MyMesh::FaceVertexIter fv_it = mesh.fv_begin(f_it.handle()); fv_it; ++fv_it)
-            {
-                MyMesh::Point point=mesh.point(fv_it);
-                face.push_back({point[0],point[1],point[2]});
-                i++;
-            }
-            sum2+= determinant(face[0],face[1],face[2]);
-        }
+int main(int argc, char *argv[]){
 
+    vector<float> *LOD = new vector<float>();
+    std::string filename = argv[1];
+
+    for (int i =2;i<argc;i++){
+        //
+        float lodpercent=atof(argv[i]);
+        LOD->push_back(lodpercent);
+    }
+
+    for (int i =0;i<LOD->size();i++){
+
+        createLOD(filename,LOD->at(i));
     }
 
 
-    cout << "sum2 " <<sum2 <<std::endl;
-    float diff=sum - sum2;
-cout << "difference sum - sum 2 " << diff <<std::endl;
-    mesh.garbage_collection();
-
-    cout << decimater.mesh().n_vertices()<<endl;
-    std::cout << "Faces deleted " << i++ << std::endl;
-    if (!OpenMesh::IO::write_mesh(mesh, "fandiskTest.obj"))
-    {
-        std::cerr << "write error\n";
-        exit(1);
-    }
     return 0;
 }
